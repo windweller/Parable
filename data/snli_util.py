04f3bed482552a14cfd3d6a57febb16934bd34cf
windweller <leo.niecn@gmail.com>
2016-04-03 22:52:25 -0700

    updates

--- data/snli_util.py
+++ data/snli_util.py
@@ -10,5 +10,34 @@ def load_data(path_to_npz):
     Solver will take this object
     and work it out inside
     """
-    # ['idx_word_map', 'data', 'W_embed', 'word_idx_map']
-    return np.load(path_to_npz)
+    # ['train_sentences', 'W_embed', 'dev_sentences', 'test_sentences',
+    # 'y_dev', 'y_train', 'word_idx_map', 'y_test', 'idx_word_map']
+    data = np.load(path_to_npz)
+
+    # we need to store those into Python variables (into memory)
+
+    print "loading training sentences..."
+    train_sentences = data['train_sentences']
+
+    print "loading dev sentences..."
+    dev_sentences = data['dev_sentences']
+
+    print "loading test sentences..."
+    test_sentences = data['test_sentences']
+
+    W_embed = data['W_embed']
+
+    y_dev = data['y_dev']
+    y_test = data['y_test']
+    y_train = data['y_train']
+
+    word_idx_map = data['word_idx_map']
+    idx_word_map = data['idx_word_map']
+
+    return {
+        'train_sentences': train_sentences,
+        'W_embed': W_embed, 'dev_sentences': dev_sentences,
+        'test_sentences': test_sentences,
+        'y_dev': y_dev, 'y_train': y_train, 'word_idx_map': word_idx_map,
+        'y_test': y_test, 'idx_word_map': idx_word_map
+    }
---
+++ numerical/num_checks.py
@@ -0,0 +1,6 @@
+"""
+This file stores numerical checks
+that are implemented in Numpy
+as an examination on whether Theano
+implementation is correct
+"""
