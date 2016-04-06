b13210771161b10f6f78c40ff6f2065ae8720f21
windweller <leo.niecn@gmail.com>
2016-04-05 23:46:06 -0700

    finished processing

--- data/sentiment_processing.py
+++ data/sentiment_processing.py
@@ -5,10 +5,11 @@ it preprocesses rt-polarity
 convert it into a npz file similar
 to SNLI
 """
+import sys
 
 """
 trim down GoogleNews-vector
-based on the words in SNLI
+based on the partial IMDB data corpus in CNN_Sentence
 """
 
 import os, json
@@ -18,58 +19,78 @@ import re
 from gensim.models.word2vec import Word2Vec
 import time
 
-label_idx_map = {'-': 0, 'entailment': 1, 'neutral': 2, 'contradiction': 3}
+label_idx_map = {'neg': 0, 'pos': 1}
 
 word_idx_map = {'<NULL>': 0, '<UNK>': 1, '<END>': 2}
 
 idx_word_map = ['<NULL>', '<UNK>', '<END>']
 
 # we replace rare words with <UNK>, which shares the same vector
-word_count_map = {}  # length: 34044
+word_count_map = {}  # length: 18768
 
 W_embed = None
 
 
-def load_dataset(base_dir):
+def load_dataset(base_dir, dev_portion=0.05, test_portion=0.05):
     data = {}
 
-    train_file = os.path.join(base_dir, 'snli_1.0_train.jsonl')
+    pos_sens_f = os.path.join(base_dir, 'rt-polarity.pos')
 
-    get_data('train', train_file, data)
+    neg_sens_f = os.path.join(base_dir, 'rt-polarity.neg')
 
-    dev_file = os.path.join(base_dir, 'snli_1.0_dev.jsonl')
+    pos_sens = get_data('pos', pos_sens_f)  # 5331 about 20% of the IMDB database (25,000)
+    neg_sens = get_data('pos', neg_sens_f)  # 5331
+    # so about 40% in total of IMDB database
 
-    get_data('dev', dev_file, data)
+    # now we convert real word sentences into indices
+    pos_sens['sentences'] = convert_words_to_idx(pos_sens['sentences'])
+    neg_sens['sentences'] = convert_words_to_idx(neg_sens['sentences'])
 
-    test_file = os.path.join(base_dir, 'snli_1.0_test.jsonl')
+    pos_choices = np.arange(len(pos_sens['sentences']))
+    neg_choices = np.arange(len(neg_sens['sentences']))
 
-    get_data('test', test_file, data)
+    # then we shuffle them
+    np.random.shuffle(pos_choices)
+    np.random.shuffle(neg_choices)
+
+    val_cut_off = int(np.ceil(len(pos_sens['sentences']) * dev_portion))
+    test_cut_off = int(np.ceil(len(pos_sens['sentences']) * test_portion) + val_cut_off)
+    # add up to 5331
+
+    # split based on proportion
+    data['X_val'] = pos_sens['sentences'][:val_cut_off] + neg_sens['sentences'][:val_cut_off]
+    data['y_val'] = np.append(pos_sens['y'][:val_cut_off], neg_sens['y'][:val_cut_off])
+
+    data['X_test'] = pos_sens['sentences'][val_cut_off:test_cut_off] + neg_sens['sentences'][val_cut_off:test_cut_off]
+    data['y_test'] = np.append(pos_sens['y'][val_cut_off:test_cut_off], neg_sens['y'][val_cut_off:test_cut_off])
+
+    # the rest, starting at test_cut_off, is all training data
+    data['X_train'] = pos_sens['sentences'][test_cut_off:] + neg_sens['sentences'][test_cut_off:]
+    data['y_train'] = np.append(pos_sens['y'][test_cut_off:], neg_sens['y'][test_cut_off:])
 
     return data
 
 
-def get_data(category, file_path, data):
+def get_data(category, file_path):
     """
     Args:
-        category: 'train', 'dev', or 'test'
+        category: 'pos', 'neg'
         data: pass in the dictionary, and we fill it up inside this function
     """
-    data[category + '_sentences'] = []
-    data['y_' + category] = []
+
+    data = {}
+
+    data['sentences'] = []
+    data['y'] = []
 
     with open(file_path, 'r') as f:
         for line in f:
-            json_obj = json._default_decoder.decode(line)
-            if json_obj['gold_label'] == '-':  # skipping non-label
-                continue
-            pair = {}
-            pair['sentence1'] = clean_str(json_obj['sentence1'])
-            pair['sentence2'] = clean_str(json_obj['sentence2'])
+            rev = []
+            rev.append(line.strip())
+            cleaned_sen = clean_str(" ".join(rev)).strip().split()
+            data['sentences'].append(cleaned_sen)
 
-            sentence1_array = pair['sentence1'].split()
-            sentence2_array = pair['sentence2'].split()
-
-            for word in sentence1_array:
+            for word in cleaned_sen:
                 if word not in word_idx_map:
                     word_idx_map[word] = len(word_idx_map)
                     idx_word_map.append(word)
@@ -78,19 +99,12 @@ def get_data(category, file_path, data):
                 else:
                     word_count_map[word] += 1
 
-            for word in sentence2_array:
-                if word not in word_idx_map:
-                    word_idx_map[word] = len(word_idx_map)
-                    idx_word_map.append(word)
-                if word not in word_count_map:
-                    word_count_map[word] = 0
-                else:
-                    word_count_map[word] += 1
+            data['y'].append(int(label_idx_map[category]))
 
-            data['y_' + category].append(int(label_idx_map[json_obj['gold_label']]))
-            data[category + '_sentences'].append(pair)
+        # data['sentences'] = np.asarray(data['sentences'])
+        data['y'] = np.asarray(data['y'], dtype='int32')
 
-    data['y_' + category] = np.asarray(data['y_' + category], dtype='int32')
+    return data
 
 
 def decode(unicode_str):
@@ -107,22 +121,19 @@ def convert_words_to_idx(data_X):
 
     Returns:
     """
-    for pair in data_X:
-        sentence1_idx = []
-        sentence2_idx = []
+    converted = []
 
-        for word in decode(pair['sentence1']).split():
-            sentence1_idx.append(word_idx_map[word])
+    for sen in data_X:
+        sen_idx = []
 
-        sentence1_idx.append(2)  # append <END> token to it
+        for word in sen:
+            sen_idx.append(word_idx_map[word])
 
-        for word in decode(pair['sentence2']).split():
-            sentence2_idx.append(word_idx_map[word])
+            sen_idx.append(2)  # append <END> token to it
 
-        sentence2_idx.append(2)  # append <END> token to it
+        converted.append(sen_idx)
 
-        pair['sentence1'] = sentence1_idx
-        pair['sentence2'] = sentence2_idx
+    return converted
 
 
 def compress_word2vec(W_embed, model):
@@ -201,32 +212,24 @@ if __name__ == '__main__':
 
     print "word2vec loaded..."
 
-    data = load_dataset(pwd + "/snli_1.0")
+    data = load_dataset(pwd)
 
     print "data loaded..."
 
-    # plan: we map words that appears less than 5 times to a special token <UNK>
-    # words that are frequent, more than 5 times, and not in word2vec,
-    # we add a random vector to it.
-
     # initialize all embeddings randomly, then we swap out
-    # words that appear in Word2Vec
+    # words that appear in Word2Vec (unseen words are initialized randomly)
 
     W_embed = np.random.randn(len(idx_word_map), 300)
 
     W_embed /= 100
 
-    convert_words_to_idx(data['train_sentencegits'])
-    convert_words_to_idx(data['dev_sentences'])
-    convert_words_to_idx(data['test_sentences'])
-
     compress_word2vec(W_embed, model)
 
-    np.savez_compressed(pwd + "/snli_processed", W_embed=W_embed,
+    np.savez_compressed(pwd + "/rt_sentiment_processed", W_embed=W_embed,
                         word_idx_map=word_idx_map, idx_word_map=idx_word_map,
-                        train_sentences=data['train_sentences'],
-                        dev_sentences=data['dev_sentences'],
-                        test_sentences=data['test_sentences'],
+                        X_train=data['X_train'],
+                        X_val=data['X_val'],
+                        X_test=data['X_test'],
                         y_train=data['y_train'],
                         y_dev=data['y_dev'],
                         y_test=data['y_test'])
@@ -235,5 +238,3 @@ if __name__ == '__main__':
 
     print "time spent: ", (end - begin), "s"
     # 271.000173807 s
-
-
